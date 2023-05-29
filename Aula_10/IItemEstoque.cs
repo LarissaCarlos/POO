ac3661c2f5c8dc43f0b5b40a9987ab1b90d5b261
LarissaCarlos <mpkavolt.l@rede.ulbra.br>
2023-05-29 20:36:36 -0300

    ap2

---
+++ Aula_10/IItemEstoque.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aula_10
+{
+    public interface IItemEstoque
+    {
+        
+    }
+}
---
+++ Aula_12/Data/Context/MeuContexto.cs
@@ -0,0 +1,9 @@
+
+namespace Aula_12.Data.Context
+{
+    public class MeuContexto : DbContext
+    
+    {
+        
+    }
+}
